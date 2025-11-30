4787d4aa06ad550112d9f58445a67c44cd38dc12
David Nakov <nakovdavid98@gmail.com>
2025-11-30 15:46:54 +0200

    Refactor: restructure Terraform configuration and remove obsolete files

---
+++ bootstrap/main.tf
@@ -0,0 +1,22 @@
+resource "azurerm_resource_group" "tfstate" {
+  name     = "rg-tfstate"
+  location = "westeurope"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "tfstate${random_id.suffix.hex}"
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
---
+++ bootstrap/provider.tf
@@ -0,0 +1,5 @@
+provider "azurerm" {
+  features {}
+  resource_provider_registrations = "none"
+  subscription_id                 = "5a18b043-6235-475b-9d4e-9ec800627e19"
+}
