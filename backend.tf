c1b181605625b931a05d54c06e55989ff53429ea
DavidNakov <55579934+NakovD@users.noreply.github.com>
2025-11-30 15:34:44 +0200

    Set up AzureRM backend in backend.tf
    Configure Terraform backend to use AzureRM.

--- backend.tf
+++ backend.tf
@@ -1,7 +1,5 @@
-provider "azurerm" {
-  features {}
-  resource_provider_registrations = "none"
-  subscription_id                 = "5a18b043-6235-475b-9d4e-9ec800627e19"
+terraform {
+  backend "azurerm" {}
 }
 
 resource "azurerm_resource_group" "tfstate" {
